6a152ffbf5a9acd8d0b13b6207a2c5130e9cf949
Safan <AbdallahSafan05@gmail.com>
2025-09-20 19:42:43 +0300

    email fix

--- utils/mailer.js
+++ utils/mailer.js
@@ -9,8 +9,10 @@ const transporter = nodemailer.createTransport({
 });
 export async function sendMail({ to, subject, text, html }) {
   const from = process.env.EMAIL_USER;
+  console.log(from);
+  console.log(to, subject, text, html);
   if (!from) throw new Error('EMAIL_USER must be set');
-  return transporter.sendMail({ from, to, subject, text, html });
+  return await transporter.sendMail({ from, to, subject, text, html });
 }
 
 export default { sendMail };
