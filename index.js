b7fae638b03271a39cffa44b444a08b38867f78f
Safan <AbdallahSafan05@gmail.com>
2025-09-20 18:56:58 +0300

    cors origins & Rate Limit

--- index.js
+++ index.js
@@ -16,19 +16,19 @@ const __dirname = path.dirname(__filename);
 
 const app = express();
 const frontendUrl = process.env.FRONTEND_URL || "http://localhost:5173"; // your frontend
-console.log("Frontend URL:", frontendUrl);
+const allowedOrigins = [frontendUrl, 'http://localhost:5173']; // add more if needed
 app.use(cors({
-   origin: frontendUrl,
+   origin: allowedOrigins,
   credentials: true               // allow cookies
 }));
 app.use(express.json());
 app.use(cookieParser());
 app.use(helmet());
-/*app.use(rateLimit({
+app.use(rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   max: 100, // limit each IP to 100 requests per windowMs
 }));
-*/
+
 // Swagger setup
 const swaggerDocument = YAML.load(path.join(__dirname, 'swagger.yaml'));
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
@@ -41,5 +41,4 @@ injectRoutes(app);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
-  console.log(`Swagger docs at http://localhost:${PORT}/api-docs`);
 });
