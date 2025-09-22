c070a5f8c5bbcffaa218f9ab5af46d1b7aa88774
Safan <AbdallahSafan05@gmail.com>
2025-09-22 16:20:09 +0300

    debug mail3

--- utils/mailer.js
+++ utils/mailer.js
@@ -28,8 +28,13 @@ export async function verifyTransport() {
 export async function sendMail({ to, subject, text, html }) {
   const from = process.env.MAIL_FROM || process.env.EMAIL_USER;
   if (!from) throw new Error('MAIL_FROM or EMAIL_USER must be set');
-  console.log(from,to,subject,text,html)
-  verifyTransport();
+  console.log(from,to,subject,text,html);
+    try {
+    await transporter.verify();
+    console.log('SMTP transporter verified and ready');
+  } catch (err) {
+    console.error('SMTP transporter verification failed:', err.message);
+  }
   return transporter.sendMail({ from, to, subject, text, html });
 }
 
