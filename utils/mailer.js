f684810c3aa8d3fd85507f75d6701800c9441543
Safan <AbdallahSafan05@gmail.com>
2025-09-22 20:11:53 +0300

    email fix3

--- utils/mailer.js
+++ utils/mailer.js
@@ -1,4 +1,6 @@
 import nodemailer from 'nodemailer';
+import dotenv from 'dotenv';
+dotenv.config();
 const transporter = nodemailer.createTransport({
   service: "gmail",  // or use SMTP provider
   auth: {
@@ -19,13 +21,8 @@ export async function verifyTransport() {
 export async function sendMail({ to, subject, text, html }) {
   const from = process.env.MAIL_FROM || process.env.EMAIL_USER;
   if (!from) throw new Error('MAIL_FROM or EMAIL_USER must be set');
-  console.log(from,to,subject,text,html);
-    try {
-    await transporter.verify();
-    console.log('SMTP transporter verified and ready');
-  } catch (err) {
-    console.error('SMTP transporter verification failed:', err.message);
-  }
+  console.log(from, to, subject, text, html);
+  console.log("Sending email...");
   return transporter.sendMail({ from, to, subject, text, html });
 }
 
