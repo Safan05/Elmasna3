e34c7dd8f15a6ce2813d57283c160ea3d5424be9
Safan <AbdallahSafan05@gmail.com>
2025-09-25 06:13:39 +0300

    Google OAuth v3

--- models/passportConfig.js
+++ models/passportConfig.js
@@ -7,7 +7,10 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/api/v1/auth/google/callback",
+      callbackURL:
+        process.env.NODE_ENV === "production"
+        ? "https://elmasna3-production.up.railway.app/api/v1/auth/google/callback"
+        : "http://localhost:5000/api/v1/auth/google/callback"
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
